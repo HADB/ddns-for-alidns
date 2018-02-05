685f783bb0a1c8af6e660555f66bd5dc638a8c2a
Bean <bean@monkey.run>
2018-02-05 22:53:10 +0800

    🎉 First Commit!

---
+++ run.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import json
+import logging
+from logging.handlers import TimedRotatingFileHandler
+from urllib.request import urlopen
+from aliyunsdkalidns.request.v20150109 import (AddDomainRecordRequest, DescribeDomainRecordsRequest, UpdateDomainRecordRequest)
+from aliyunsdkcore import client
+
+
+# 获取配置
+def get_config():
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+        return config
+
+
+# 从域名中获取RR和主域名
+def get_domain_parts(domain):
+    parts = domain.split('.')
+    length = len(parts)
+    if length > 2:
+        return '.'.join(parts[0:length - 2]), '.'.join(parts[length - 2:length])
+    else:
+        return '@', '.'.join(parts[0:length])
+
+
+# 获取解析记录
+def get_record(acs_client, domain_name, rr):
+    request = DescribeDomainRecordsRequest.DescribeDomainRecordsRequest()
+    request.set_DomainName(domain_name)
+    request.set_accept_format('json')
+    records = json.JSONDecoder().decode(acs_client.do_action_with_exception(request).decode())['DomainRecords']['Record']
+    for record in records:
+        if record['RR'] == rr:
+            return record['RecordId'], record['Value']
+    return None, None
+
+
+# 添加记录
+def add_record(acs_client, record_id, rr, domain_name, current_ip, ttl):
+    request = AddDomainRecordRequest.AddDomainRecordRequest()
+    request.set_RR(rr)
+    request.set_Type('A')
+    request.set_DomainName(domain_name)
+    request.set_Value(current_ip)
+    request.set_TTL(ttl)
+    request.set_accept_format('json')
+    result = acs_client.do_action_with_exception(request)
+    return result
+
+
+# 更新记录
+def update_record(acs_client, record_id, rr, domain_name, current_ip, ttl):
+    request = UpdateDomainRecordRequest.UpdateDomainRecordRequest()
+    request.set_RR(rr)
+    request.set_Type('A')
+    request.set_Value(current_ip)
+    request.set_RecordId(record_id)
+    request.set_TTL(ttl)
+    request.set_accept_format('json')
+    result = acs_client.do_action_with_exception(request)
+    return result
+
+
+# 获取外网IP
+def get_public_ip():
+    return urlopen('http://v4.ipv6-test.com/api/myip.php', timeout=60).read().decode()
+
+
+# 主函数
+def main():
+    handler = TimedRotatingFileHandler('logs/ddns-for-alidns.log', when='D', interval=1, backupCount=30)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s', '%Y-%m-%d %H:%M:%S')
+    handler.setFormatter(formatter)
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    config = get_config()
+    acs_client = client.AcsClient(config['access_key_id'], config['access_key_secret'], config['region_id'])
+    current_ip = get_public_ip()
+    for domain in config['domains']:
+        rr, domain_name = get_domain_parts(domain)
+        record_id, value = get_record(acs_client, domain_name, rr)
+        if record_id == None:
+            add_record(acs_client, record_id, rr, domain_name, current_ip, config['ttl'])
+            logging.info(domain + '添加解析为' + current_ip)
+        else:
+            if value != current_ip:
+                update_record(acs_client, record_id, rr, domain_name, current_ip, config['ttl'])
+                logging.info(domain + '更新解析为' + current_ip)
+            else:
+                logging.info(domain + '无需更新解析')
+
+
+if __name__ == '__main__':
+    main()
