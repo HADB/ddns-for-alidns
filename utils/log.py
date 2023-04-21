585aeba215b6f4bebea21b7dd6481ba8416061cd
Bean <bean@yuanfen.net>
2023-04-21 23:16:05 +0800

    Revert "debug"
    This reverts commit 934cedd3edbfa23b164388a5ce0488b520892966.

--- utils/log.py
+++ utils/log.py
@@ -8,7 +8,7 @@ log_formatter = logging.Formatter("%(asctime)s [%(levelname)-8s] %(message)s")
 
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(log_formatter)
-stream_handler.setLevel(logging.DEBUG)
+stream_handler.setLevel(logging.INFO)
 
 file_handler = TimedRotatingFileHandler(
     "logs/ddns-for-alidns",
@@ -19,10 +19,10 @@ file_handler = TimedRotatingFileHandler(
 )
 file_handler.suffix = "%Y%m%d.log"
 file_handler.setFormatter(log_formatter)
-file_handler.setLevel(logging.DEBUG)
+file_handler.setLevel(logging.INFO)
 
 logger = logging.getLogger()
-logger.setLevel(level=logging.DEBUG)
+logger.setLevel(level=logging.INFO)
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
